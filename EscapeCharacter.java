8cf36c3c789e1ee1eae6b55acbdd35417397b931
OkirieChukwumeniemFaith1 <chukwumeniemfo@gmail.com>
2026-05-11 11:14:01 +0100

    Third Assignment

---
+++ EscapeCharacter.java
@@ -0,0 +1,10 @@
+public class EscapeCharacter{
+	public static void main(String[] arges) {
+		System.out.print("When you need to use the escape characters \n");
+		System.out.print("Use it when you want to manipulate your cursor");
+		System.out.print("Use \\n when you want a newline \n");
+		System.out.print("Who is the father of java?. The father of java is \"James Gosling\"");
+		System.out.print("\tUse it when you want a tab or indentation");
+		System.out.print("We are using carriage return here");
+    }
+}
---
+++ PrimitiveDataType.java
@@ -0,0 +1,18 @@
+public class PrimitiveDataType{
+	public static void main(String[] args){
+		byte myAge = 30;
+		System.out.printf("miss Mercy is %d years old%n",myAge);
+		
+		short quantity = 20000;
+		System.out.printf("The quantity of iphones ordered is %,d%n",quantity);
+		
+		int nigeriaPopulation = 294848848;
+		System.out.printf("This population of Nigeria is %,d%n",nigeriaPopulation);
+
+		long worldPopulation = 47474778478474874L;
+		System.out.printf("This world's population is %,d%n",worldPopulation);
+		
+		float price = 577858.84747;
+		System.out.println("The price of each iphone per unit is %,.2f%n",Price); 
+	}	
+}
